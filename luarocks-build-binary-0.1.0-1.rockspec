40278b8c3e2bcb03980b4b65ed5e4c7227e38230
Vhyrro <vhyrro@gmail.com>
2024-01-02 20:04:41 +0100

    feat: initial commit

---
+++ luarocks-build-binary-0.1.0-1.rockspec
@@ -0,0 +1,14 @@
+rockspec_format = "3.0"
+package = "luarocks-build-binary"
+version = "0.1.0-1"
+
+source = {
+    url = "git+https://github.com/vhyrro/luarocks-build-binary",
+    tag = "0.1.0",
+}
+
+description = {
+    summary = "TODO",
+    homepage = "https://github.com/vhyrro/luarocks-build-binary",
+    license = "GPLv3",
+}
---
+++ src/luarocks/build/rust-build.lua
@@ -0,0 +1,70 @@
+-- NOTE: This file takes the code from [here](https://github.com/khvzak/luarocks-build-rust-mlua/blob/main/src/luarocks/build/rust-mlua.lua)
+-- as a template!
+
+local fs = require("luarocks.fs")
+local cfg = require("luarocks.core.cfg")
+local dir = require("luarocks.dir")
+local path = require("luarocks.path")
+
+local rust_build = {}
+
+function rust_build.run(rockspec)
+    assert(rockspec:type() == "rockspec")
+
+    if not fs.is_tool_available("cargo", "Cargo") then
+        return nil, "'cargo' is not installed.\n" .. "This rock uses a binary written in Rust: make sure you have a Rust\n" ..
+            "development environment installed and try again."
+    end
+
+    local features = {}
+    local cmd = {"cargo install --root . --bins"}
+
+    if not rockspec.build then
+        return
+    end
+
+    -- Add additional features
+    if type(rockspec.build.features) == "table" then
+        for _, feature in ipairs(rockspec.build.features) do
+            table.insert(features, feature)
+        end
+    elseif type(rockspec.build.features) == "string" then
+        table.insert(features, rockspec.build.features)
+    end
+
+    assert(type(rockspec.build.binary) == "string", "Expected a `build.binary` variable of type string in the rockspec!")
+
+    table.insert(cmd, rockspec.build.binary)
+
+    if not fs.execute(table.concat(cmd, " ")) then
+        return nil, "Installation failed."
+    end
+
+    local ext = cfg.external_deps_patterns.bin
+
+    local bin_dir = path.bin_dir(rockspec.name, rockspec.version)
+    local ok, err = fs.make_dir(bin_dir)
+
+    if not ok then
+        return nil, "Failed to create installation directory: " .. err
+    end
+
+    for _, extension in ipairs(ext) do
+        extension = extension:match("^.*%?(%..+)$")
+
+        local binary_path = dir.path("bin", rockspec.build.binary, extension)
+
+        if fs.exists(binary_path) then
+
+            ok, err = fs.copy(binary_path, dir.path(bin_dir, rockspec.build.binary, extension), "exec")
+
+            if not ok then
+                return nil, "Failed installing " .. binary_path .. " in " .. bin_dir .. ": " .. err
+            end
+        end
+    end
+
+    return true
+end
+
+return rust_build
